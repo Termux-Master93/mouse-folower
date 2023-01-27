e31e8b1a6e26da98c84a448143ec9e075d8ea746
Termux-Master93 <brisantur8034@gmail.com>
2023-01-27 14:51:06 -0500

    agregar diseño de circulo

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,26 @@ function App() {
   },[enabled])
 
   return (
-    <>
-      <p>Mosue Follower</p>
+    <main>
+      <div
+        style={{
+          position: 'absolute',
+          backgroundColor: '#09f',
+          borderRadius: '50%',
+          opacity: '0.8',
+          pointerEvents: 'none',
+          left: -20,
+          top: -20,
+          width: 40,
+          height: 40,
+          transform: 'translate(0px,0px)'
+        }
+        }
+      />
       <button
         onClick={()=>setEnabled(!enabled)}
       >{enabled ? 'Desactivar' : 'Activar'} Seguir Puntero</button>
-    </>
+    </main>
   )
 }
 
